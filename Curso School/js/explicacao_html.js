65ff33ab4e3898b3d01a8eaaf1bd5261cbb96a0d
Vitor1973 <devechi.vitor@gmail.com>
2023-03-09 20:58:57 -0300

    09/03

---
+++ Curso School/js/explicacao_html.js
@@ -0,0 +1,11 @@
+/*
+https://www.w3schools.com/jsref/met_document_queryselector.asp
+Documento HTML DOM querySelector()
+
+Definição e Uso
+O querySelector()método retorna o primeiro elemento que corresponde a um seletor CSS.
+Para retornar todas as correspondências (não apenas a primeira), use o querySelectorAll()em vez disso.
+Ambos querySelector()e querySelectorAll()lançam uma exceção SYNTAX_ERR se o(s) seletor(es) for(em) inválido(s).
+
+
+*/
